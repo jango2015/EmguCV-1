5303a7f7e6f9b719a7b5fb5c4a9d389185065258
Senatauro <cgsste@hotmail.com>
2016-06-03 20:12:00 -0300

    EmguCV
    Criando um programinha usando EmguCV, uma biblioteca de "Computer
    Vision" baseado em cima da Open CV. De momento ele só abre o programa,
    reconhece uma webcam, caso possua uma, pega os frames dela e tenta
    reconhecer uma face.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace EmguCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.iniciarBT = new System.Windows.Forms.Button();
+            this.salvarImagem = new System.Windows.Forms.Button();
+            this.comboBoxEscala = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBoxMinViz = new System.Windows.Forms.ComboBox();
+            this.txtFacesIdentificadas = new System.Windows.Forms.TextBox();
+            this.numFacesIdentificadas = new System.Windows.Forms.TextBox();
+            this.textBoxMinJanela = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(703, 371);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // iniciarBT
+            // 
+            this.iniciarBT.Location = new System.Drawing.Point(593, 389);
+            this.iniciarBT.Name = "iniciarBT";
+            this.iniciarBT.Size = new System.Drawing.Size(122, 23);
+            this.iniciarBT.TabIndex = 3;
+            this.iniciarBT.Text = "Iniciar captura de tela";
+            this.iniciarBT.UseVisualStyleBackColor = true;
+            this.iniciarBT.Click += new System.EventHandler(this.iniciarBT_Click);
+            // 
+            // salvarImagem
+            // 
+            this.salvarImagem.Location = new System.Drawing.Point(12, 389);
+            this.salvarImagem.Name = "salvarImagem";
+            this.salvarImagem.Size = new System.Drawing.Size(91, 23);
+            this.salvarImagem.TabIndex = 4;
+            this.salvarImagem.Text = "Salvar Imagem";
+            this.salvarImagem.UseVisualStyleBackColor = true;
+            this.salvarImagem.Click += new System.EventHandler(this.salvarImagem_Click);
+            // 
+            // comboBoxEscala
+            // 
+            this.comboBoxEscala.FormattingEnabled = true;
+            this.comboBoxEscala.Location = new System.Drawing.Point(825, 18);
+            this.comboBoxEscala.Name = "comboBoxEscala";
+            this.comboBoxEscala.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEscala.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(721, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Taxa de escala";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(721, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Minimo de vizinhos";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(721, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Min pixels para checar";
+            // 
+            // comboBoxMinViz
+            // 
+            this.comboBoxMinViz.FormattingEnabled = true;
+            this.comboBoxMinViz.Location = new System.Drawing.Point(825, 45);
+            this.comboBoxMinViz.Name = "comboBoxMinViz";
+            this.comboBoxMinViz.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMinViz.TabIndex = 9;
+            // 
+            // txtFacesIdentificadas
+            // 
+            this.txtFacesIdentificadas.Location = new System.Drawing.Point(721, 222);
+            this.txtFacesIdentificadas.Name = "txtFacesIdentificadas";
+            this.txtFacesIdentificadas.ReadOnly = true;
+            this.txtFacesIdentificadas.Size = new System.Drawing.Size(153, 20);
+            this.txtFacesIdentificadas.TabIndex = 11;
+            this.txtFacesIdentificadas.Text = "Numero de faces identificadas";
+            this.txtFacesIdentificadas.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // numFacesIdentificadas
+            // 
+            this.numFacesIdentificadas.Location = new System.Drawing.Point(880, 222);
+            this.numFacesIdentificadas.Name = "numFacesIdentificadas";
+            this.numFacesIdentificadas.Size = new System.Drawing.Size(66, 20);
+            this.numFacesIdentificadas.TabIndex = 12;
+            // 
+            // textBoxMinJanela
+            // 
+            this.textBoxMinJanela.Location = new System.Drawing.Point(880, 72);
+            this.textBoxMinJanela.Name = "textBoxMinJanela";
+            this.textBoxMinJanela.Size = new System.Drawing.Size(66, 20);
+            this.textBoxMinJanela.TabIndex = 13;
+            this.textBoxMinJanela.Text = "25";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 416);
+            this.Controls.Add(this.textBoxMinJanela);
+            this.Controls.Add(this.numFacesIdentificadas);
+            this.Controls.Add(this.txtFacesIdentificadas);
+            this.Controls.Add(this.comboBoxMinViz);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxEscala);
+            this.Controls.Add(this.salvarImagem);
+            this.Controls.Add(this.iniciarBT);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button iniciarBT;
+        private System.Windows.Forms.Button salvarImagem;
+        private System.Windows.Forms.ComboBox comboBoxEscala;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBoxMinViz;
+        private System.Windows.Forms.TextBox txtFacesIdentificadas;
+        private System.Windows.Forms.TextBox numFacesIdentificadas;
+        private System.Windows.Forms.TextBox textBoxMinJanela;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace EmguCV
+{
+    public partial class Form1 : Form
+    {
+        private Capture capturaDaCamera;        //pega imagens da camera como frames
+        private bool capturaEmProgresso;        //checa se a captura esta sendo executada
+        private HaarCascade haar;
+        private int tamanhoDaJanela = 25;
+        private double fatorDeEscala = 1.1;
+        private int numeroMinimoDeVizinhos = 4;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            haar = new HaarCascade(Environment.CurrentDirectory + "\\haarcascade_frontalface_alt_tree.xml");
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessarFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> frame = capturaDaCamera.QueryFrame();
+            imageBox1.Image = frame;
+            if (frame != null)
+            {
+                //converte a imagem para uma escala em cinza
+                Image<Gray, byte> frameCinza = frame.Convert<Gray, byte>();
+
+                //numeroMinimoDeVizinhos = int.Parse(comboBoxMinViz.SelectedText);
+                //fatorDeEscala = int.Parse(comboBoxEscala.SelectedText);
+                //tamanhoDaJanela = int.Parse(textBoxMinJanela.Text);
+
+
+                //detecta faces apartir da imagem em cinza e guarda elas em um array do tipo var
+                var faces =  frameCinza.DetectHaarCascade(haar, 1.1, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
+                int TotalFaces = faces.Length;
+                numFacesIdentificadas.Text = TotalFaces.ToString();
+               // MessageBox.Show("Total faces detected: " + TotalFaces.ToString());
+                foreach (var face in faces)
+                {
+                    frame.Draw(face.rect, new Bgr(Color.Green), 3);
+                }
+            }
+            imageBox1.Image = frame;
+        }
+
+        /*
+        Para mostrar a quantidade de faces identificadas em uma chamada de x funcao:
+        int TotalFaces = faces.Length;
+        MessageBox.Show("Total faces detected: " + TotalFaces.ToString());
+        */
+        
+        //Mostra a imagem em Windows Form Picturebox
+        //pictureBox1.Image = ImageFrame.ToBitmap();
+        //Para salvar a imagem: Frame.Save(@"E:\MyPic.jpg");
+
+        private void iniciarBT_Click(object sender, EventArgs e)
+        {
+            
+            #region if capturaDaCamera is not created, create it now
+            if (capturaDaCamera == null)
+            {
+                try
+                {
+                    capturaDaCamera = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capturaDaCamera != null)
+            {
+                if (capturaEmProgresso)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    iniciarBT.Text = "Iniciar!"; //
+                    Application.Idle -= ProcessarFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    iniciarBT.Text = "Parar";
+                    Application.Idle += ProcessarFrame;
+                }
+
+                capturaEmProgresso = !capturaEmProgresso;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (capturaDaCamera != null)
+                capturaDaCamera.Dispose();
+        }
+
+        private void salvarImagem_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> Imagem = capturaDaCamera.QueryFrame();
+            Imagem.Save(Environment.CurrentDirectory + "\\imagem.jpg");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
